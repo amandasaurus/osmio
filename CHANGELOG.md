508c06068d259cfcd125324d94e8183d431a755b
Rory McCann <rory@technomancy.org>
2020-01-06 18:00:30 +0100

    ISO timeformats nicer, without ms

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# Unreleased
+
+* ISO timeformats are more clearer without milliseconds
+
 # v0.1.0 (2019-10-06)
 
 * Initial work
--- src/utils.rs
+++ src/utils.rs
@@ -12,7 +12,7 @@ macro_rules! try_opt {
 pub fn epoch_to_iso(epoch: i32) -> String {
     // Chrono is a little silly and can't easily convert from epoch to utc timezone
     let d: DateTime<Utc> = DateTime::from_utc(NaiveDateTime::from_timestamp(epoch as i64, 0), Utc);
-    d.to_rfc3339()
+    d.to_rfc3339_opts(chrono::SecondsFormat::Secs, true)
 }
 
 pub fn iso_to_epoch(iso: &str) -> u32 {
