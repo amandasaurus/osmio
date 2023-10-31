bae58e32533c97c774ad985f4c705234ce48e79c
Amanda McCann <amanda@technomancy.org>
2023-10-31 18:06:18 +0100

    raw copy of arcpbf to new stringpbf

---
+++ src/stringpbf/test.rs
@@ -0,0 +1,18 @@
+#![cfg(test)]
+use std::fs;
+use std::path::Path;
+use super::super::OSMReader;
+
+use super::PBFReader;
+
+#[test]
+fn test_file() {
+    //let file = fs::File::open(&Path::new("/home/rory/code/rust/osmio/monaco-latest.osm.pbf")).unwrap();
+    //let mut reader = PBFReader::new(file);
+
+    //for obj in reader.objects() {
+    //    //println!("{:?}", obj);
+    //}
+
+
+}
